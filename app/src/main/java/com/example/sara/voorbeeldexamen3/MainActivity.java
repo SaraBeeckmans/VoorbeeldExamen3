a2ab940b3d24f16709befdd5688ef679348681aa
Sara Beeckmans <sara@sarabeeckmans.be>
2019-01-21 13:02:53 +0100

    werkend met notificatie

--- app/src/main/java/com/example/sara/voorbeeldexamen3/MainActivity.java
+++ app/src/main/java/com/example/sara/voorbeeldexamen3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sara.voorbeeldexamen3;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private Long differenz=0l;
     private int clicked = 0;
 
+    //notificaties
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
 
 
     @Override
@@ -22,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        createNotificationChannel();
 
     }
 
@@ -33,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         if(clicked == 2){
             endtime = System.currentTimeMillis();
             differenz= (endtime-starttime);
+            sendNotification();
 
         }
     }
@@ -50,4 +60,39 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, ListHighScore.class);
         startActivity(i);
     }
+
+    public void createNotificationChannel()
+    {
+        mNotifyManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+            // Create a NotificationChannel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager
+                    .IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("New score")
+                .setContentText("Je nieuwe score is: "+ Long.toString(differenz)+ " ms")
+                .setSmallIcon(R.drawable.ic_launcher_foreground);
+        return notifyBuilder;
+
+    }
+
+    public void sendNotification() {
+        getNotificationBuilder();
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
 }
